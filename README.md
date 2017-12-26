17508253e423d32408c7e175c5d173ca8c9c47df
张龙 <zhangl@jiagouyun.com>
2017-12-26 17:18:53 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# my-echarts
+###项目信息：
+- jq+es6+echarts
+- 使用：echarts、webpack
+- 目录结构：开发目录/src, 编译目录/dist
+- 开发环境启动：npm run dev
+
+---
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const webpack = require('webpack')
+const echarts = require('echarts')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  context: path.resolve(__dirname, './'),
+
+  entry: {
+    app: './src/app.js'
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: '[name].js'
+  },
+
+  devServer: {
+    contentBase: "./public",
+    historyApiFallback: true,
+    inline: true,
+    hot: true
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /^node_modules$/,
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader',
+        exclude: /^node_modules$/,
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000
+        }
+      }
+    ]
+  },
+
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      _: 'lodash'
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      inject: true,
+      favicon: path.resolve(__dirname, './favicon.ico')
+    })
+  ]
+  
+}
